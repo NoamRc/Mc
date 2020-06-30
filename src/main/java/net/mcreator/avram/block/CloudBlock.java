6cafaf79decd10ba9efd09a9d7805ad3d6a73fad
NoamRc <59398204+NoamTheKing13@users.noreply.github.com>
2020-06-30 22:07:50 +0300

    Cloud

---
+++ src/main/java/net/mcreator/avram/block/CloudBlock.java
@@ -0,0 +1,50 @@
+
+package net.mcreator.avram.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.avram.AvramModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AvramModElements.ModElement.Tag
+public class CloudBlock extends AvramModElements.ModElement {
+	@ObjectHolder("avram:cloud")
+	public static final Block block = null;
+	public CloudBlock(AvramModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.SPONGE).sound(SoundType.SLIME).hardnessAndResistance(0.05f, 0f).lightValue(0));
+			setRegistryName("cloud");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(Blocks.AIR, (int) (0)));
+		}
+	}
+}
